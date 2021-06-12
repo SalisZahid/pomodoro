b04d5794d5338190a19766c67bce4c29c2ef60b6
Salis Zahid <saliszahid@ucp.edu.pk>
2021-06-12 21:41:07 +0500

    firstCommit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
